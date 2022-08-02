9fe9baface98f97bda138583a6f879a485e2eae5
AIKYN <kairatnurmakhan>
2022-08-02 20:08:19 +0600

    make it better

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ client.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
 admin.register_handlers_admin(dp)
 fsmAdminMenu.register_handlers_fsmAdminMenu(dp)
-inline.inline_wiki_handler(dp)
+inline.register_handlers_inline(dp)
 notification.register_handler_notification(dp)
 
 extra.register_handlers_extra(dp)
