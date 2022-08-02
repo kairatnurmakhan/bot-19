fca298dbc3e73ebce715e0cd018d10e80c12f890
AIKYN <kairatnurmakhan>
2022-08-02 16:18:12 +0600

    make it better

---
+++ handlers/inline.py
@@ -0,0 +1,34 @@
+import hashlib
+from pprint import pprint
+
+from aiogram import types, Dispatcher
+from youtube_search import YoutubeSearch as YT
+
+
+def finder(text):
+    results = YT(text, max_results=10).to_dict()
+    return results
+
+
+pprint(finder("slivki"))
+
+
+
+async def inline_wiki_handler(query: types.InlineQuery):
+    text = query.query or "echo"
+    link = f"https://www.wikipedia.org/search?q={text}"
+    result_id: str = hashlib.md5(text.encode()).hexdigest()
+    articles = [types.InlineQueryResultArticle(
+        id=result_id,
+        title="wikipedia: ",
+        url=link,
+        input_message_content=types.InputMessageContent(
+            message_text=link
+        )
+    )]
+    await query.answer(articles, cache_time=60, is_personal=True)
+
+
+def register_handlers_inline(dp: Dispatcher):
+    # dp.register_inline_handler(inline_youtube_handler)
+    dp.register_inline_handler(inline_wiki_handler)
